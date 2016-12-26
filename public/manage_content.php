f5e6bcf548a684eb223e9913c9fd3f1fb351916f
aprietof <adrianprieto00@gmail.com>
2016-12-26 16:39:04 -0500

    pages added into subjects

--- public/manage_content.php
+++ public/manage_content.php
@@ -2,7 +2,7 @@
 <?php require_once("../includes/functions.php"); // FUNCTIONS FILE ?>
 
 <?php
-  // PERFORM DB QUERY
+  // PERFORM SUBJECTS DB QUERY
   $query = "SELECT * FROM subjects WHERE visible = 1";
   $result = mysqli_query($db, $query); #=> collection of database rows
 
@@ -20,9 +20,28 @@
         // USE RETURNED DATA (IF ANY)
         while ($subject = mysqli_fetch_assoc($result)) { // increment the pointer
           // output data from each row
-          echo "<li>{$subject["menu_name"]} - ({$subject["id"]})</li>";
+          echo "<li>{$subject["menu_name"]}";
+
+          // PERFORM PAGES DB QUERY
+          $query = "SELECT * FROM pages WHERE visible = 1 AND subject_id = {$subject["id"]} ORDER BY position ASC";
+          $page_set = mysqli_query($db, $query); #=> collection of database rows
+
+          // Test if there was a query error
+          confirm_query($page_set);
+          echo "<ul class='pages'>";
+
+          while ($page = mysqli_fetch_assoc($page_set)) { // increment the pointer
+            // output data from each row
+            echo "<li>{$page["menu_name"]}</li>";
+          }
+
+          mysqli_free_result($page_set); // RELEASE PAGES RETURNED DB
+
+          echo "</ul>";
+          echo "</li>";
         }
       ?>
+      <?php mysqli_free_result($result); // RELEASE RETURNED DB ?>
     </ul>
 
   </div>
@@ -32,5 +51,4 @@
   </div>
 </div>
 
-<?php mysqli_free_result($result); // RELEASE RETURNED DB ?>
 <?php include("../includes/layouts/footer.php"); // FOOTER ?>

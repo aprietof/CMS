a3ef1db992dd90f8386a991e198b621189102438
aprietof <adrianprieto00@gmail.com>
2016-12-27 01:15:52 -0500

    navigation refactored into a function

--- includes/functions.php
+++ includes/functions.php
@@ -37,6 +37,45 @@
     return $class = (isset($selected) && isset($page) && $selected == $page["id"]) ? "selected" : "";
   }
 
+  // RENDER NAVIGATION BAR (takes two arguments)
+  # the currently selected subject ID (if any)
+  # the currently selected page ID (if any)
+  function navigation($subject_id, $page_id) {
 
+    // PERFORM SUBJECTS DB QUERY
+    $subjects_set = find_all_subjects();
+    confirm_query($subjects_set);
+
+    $output = "<ul class=\"subjects\">";
+
+        // USE RETURNED DATA (IF ANY)
+        while ($subject = mysqli_fetch_assoc($subjects_set)) { // increment the pointer
+
+          // output data from each row
+          $output .= "<li class=" . selected_class($subject_id, $subject) . ">";
+          $output .= "<a href=\"manage_content.php?subject=" . urlencode($subject["id"]) . "\">{$subject["menu_name"]}</a>";
+
+          // PERFORM PAGES DB QUERY
+          $pages_set = find_all_pages($subject["id"]);
+          confirm_query($pages_set);
+
+          $output .= "<ul class=\"pages\">";
+
+          while ($page = mysqli_fetch_assoc($pages_set)) { // increment the pointer
+            // output data from each row
+            $output .= "<li class=". selected_class($page_id, $page) .">";
+            $output .= "<a href=\"manage_content.php?page=" . urlencode($page["id"]) . "\">{$page["menu_name"]}</a></li>";
+          }
+
+          mysqli_free_result($pages_set); // RELEASE PAGES RETURNED DB
+
+          $output .= "</ul>";
+          $output .= "</li>";
+        }
+
+        mysqli_free_result($subjects_set); // RELEASE RETURNED DB
+    $output .= "</ul>";
+    return $output;
+  }
 
 ?>
--- public/manage_content.php
+++ public/manage_content.php
@@ -1,63 +1,29 @@
 <?php require_once("../includes/db_connection.php"); // CREATE DB CONNECTION ?>
 <?php require_once("../includes/functions.php"); // FUNCTIONS FILE ?>
-<?php
-  // PERFORM SUBJECTS DB QUERY
-  $subjects_set = find_all_subjects();
-  confirm_query($subjects_set);
-?>
 <?php include("../includes/layouts/header.php"); // HEADER ?>
 
 <?php
   // CHECK FOR PAGE CONTENT
-  $selected_subject = NULL;
-  $selected_page = NULL;
+  $selected_subject_id = NULL;
+  $selected_page_id = NULL;
 
   if (isset($_GET["subject"])) {
-    $selected_subject = $_GET["subject"];
+    $selected_subject_id = $_GET["subject"];
   } elseif (isset($_GET["page"])) {
-    $selected_page = $_GET["page"];
+    $selected_page_id = $_GET["page"];
   }
 ?>
 
 <div id="main">
   <div id="navigation">
-
-    <ul class="subjects">
-      <?php
-        // USE RETURNED DATA (IF ANY)
-        while ($subject = mysqli_fetch_assoc($subjects_set)) { // increment the pointer
-
-          // output data from each row
-          echo "<li class=" . selected_class($selected_subject, $subject) . ">";
-          echo "<a href=\"manage_content.php?subject=" . urlencode($subject["id"]) . "\">{$subject["menu_name"]}</a>";
-
-          // PERFORM PAGES DB QUERY
-          $pages_set = find_all_pages($subject["id"]);
-          confirm_query($pages_set);
-
-          echo "<ul class=\"pages\">";
-
-          while ($page = mysqli_fetch_assoc($pages_set)) { // increment the pointer
-            // output data from each row
-            echo "<li class=". selected_class($selected_page, $page) .">";
-            echo "<a href=\"manage_content.php?page=" . urlencode($page["id"]) . "\">{$page["menu_name"]}</a></li>";
-          }
-
-          mysqli_free_result($pages_set); // RELEASE PAGES RETURNED DB
-
-          echo "</ul>";
-          echo "</li>";
-        }
-      ?>
-      <?php mysqli_free_result($subjects_set); // RELEASE RETURNED DB ?>
-    </ul>
-
+    <?php echo navigation($selected_subject_id, $selected_page_id); ?>
   </div>
+
   <div id="page">
     <h2>Manage Content</h2>
 
-      <?php echo $selected_subject; ?>
-      <?php echo $selected_page; ?>
+      <?php echo $selected_subject_id; ?>
+      <?php echo $selected_page_id; ?>
 
   </div>
 </div>
